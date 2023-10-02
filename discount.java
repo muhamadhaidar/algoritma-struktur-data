5bc18a565e5a380d943546c9b0963be4c10d9c0f
muhamadhaidar <145978283+muhamadhaidar@users.noreply.github.com>
2023-10-02 19:20:07 +0700

    Add files via upload

---
+++ discount.java
@@ -0,0 +1,18 @@
+public class discount {
+    public static void main(String[] args) throws Exception {
+        int discount = 0;
+        int belanja = 150000;
+
+        if (belanja > 100000) {
+            discount = 10*belanja/100;
+        }
+
+        int totalbayar = belanja-discount;
+        System.out.println("Total bayar: "+totalbayar);
+    }
+
+
+   
+    
+    
+}
